01e61ebee121af4d0dba45f554e7c993d80a7b23
josemarquez-oficina <jose@aplitic.com.gt>
2019-02-25 11:48:10 -0600

    Varios

--- vistas/contenidos/categoria-personalizada-vista.php
+++ vistas/contenidos/categoria-personalizada-vista.php
@@ -19,70 +19,204 @@
 				$urlBGcabecera = SERVERURL . "vistas/assets/img/banner.jpg";
 			}
 		?>
-		<div class="category-element-1 full-width" style="background-image: url('<?php echo $urlBGcabecera; ?>');">
+		<div class="category-element-1 full-width" style="background-image: url('<?php echo $urlBGcabecera; ?>');background-size: cover;background-position: center;">
 			<div class="content boxed-width">
 				<h1><?php echo $infoCat['nombre']; ?></h1>
 				<h3><?php echo $infoCat['descripcion']; ?></h3>
 			</div>
 		</div>
-		<div class="banner boxed-width">
-			<div class="ban">
-				<img src="<?php echo SERVERURL; ?>vistas/assets/img/banner.jpg" alt="Banner">
-			</div>
-		</div>
-		<div class="home-element-2 boxed-width">
+
+		<div class="category-element-2 boxed-width">
 			<div class="categories">
-				<div class="title"><h4>Categorías</h4></div>
-				<div class="categories-slideshow">
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
+				<div class="title">
+					<i class="fas fa-bars"><span>DEPARTAMENTOS</span></i>
+				</div>
+				<div class="menu">
+					<ul>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+						<li><a href="#" class="fas"><i class="fas fa-desktop"><span>Categorías</span></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="news">
+				<div class="title">
+					<p>Últimas novedades</p>
+				</div>
+				<div class="news-items">
+					<div class="news-slideshow">
+						<div class="news-item">
+							<div class="product">
+								<div class="product-image">
+									<a href="#"><img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.jpg"></a>
+								</div>
+								<div class="product-description">
+									<div class="title"><a href="#"><h3>Notebook HP HP x360 11-ab042la</h3></a></div>
+									<div class="rated">
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star gray"></i>
+									</div>
+									<div class="price">
+										<p class="currency">Q.</p>
+										<p class="units">2,345.</p>
+										<p class="decimals">98</p>
+									</div>
+									<div class="legend">
+										<p>Precio de contado</p>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
+						<div class="news-item">
+							<div class="product">
+								<div class="product-image">
+									<a href="#"><img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.jpg"></a>
+								</div>
+								<div class="product-description">
+									<div class="title"><a href="#"><h3>Notebook HP HP x360 11-ab042la</h3></a></div>
+									<div class="rated">
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star gray"></i>
+									</div>
+									<div class="price">
+										<p class="currency">Q.</p>
+										<p class="units">2,345.</p>
+										<p class="decimals">98</p>
+									</div>
+									<div class="legend">
+										<p>Precio de contado</p>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
+						<div class="news-item">
+							<div class="product">
+								<div class="product-image">
+									<a href="#"><img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.jpg"></a>
+								</div>
+								<div class="product-description">
+									<div class="title"><a href="#"><h3>Notebook HP HP x360 11-ab042la</h3></a></div>
+									<div class="rated">
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star gray"></i>
+									</div>
+									<div class="price">
+										<p class="currency">Q.</p>
+										<p class="units">2,345.</p>
+										<p class="decimals">98</p>
+									</div>
+									<div class="legend">
+										<p>Precio de contado</p>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
+						<div class="news-item">
+							<div class="product">
+								<div class="product-image">
+									<a href="#"><img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.jpg"></a>
+								</div>
+								<div class="product-description">
+									<div class="title"><a href="#"><h3>Notebook HP HP x360 11-ab042la</h3></a></div>
+									<div class="rated">
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star gray"></i>
+									</div>
+									<div class="price">
+										<p class="currency">Q.</p>
+										<p class="units">2,345.</p>
+										<p class="decimals">98</p>
+									</div>
+									<div class="legend">
+										<p>Precio de contado</p>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
+						<div class="news-item">
+							<div class="product">
+								<div class="product-image">
+									<a href="#"><img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.jpg"></a>
+								</div>
+								<div class="product-description">
+									<div class="title"><a href="#"><h3>Notebook HP HP x360 11-ab042la</h3></a></div>
+									<div class="rated">
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star gray"></i>
+									</div>
+									<div class="price">
+										<p class="currency">Q.</p>
+										<p class="units">2,345.</p>
+										<p class="decimals">98</p>
+									</div>
+									<div class="legend">
+										<p>Precio de contado</p>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
-						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
-						</div>
-					</div>
-					<div class="categories-item">
-						<div class="categorie">
-							<img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.png" alt="Categoria">
-							<h3>Categoría</h3>
+						<div class="news-item">
+							<div class="product">
+								<div class="product-image">
+									<a href="#"><img src="<?php echo SERVERURL; ?>vistas/assets/img/pc.jpg"></a>
+								</div>
+								<div class="product-description">
+									<div class="title"><a href="#"><h3>Notebook HP HP x360 11-ab042la</h3></a></div>
+									<div class="rated">
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star yellow"></i>
+										<i class="fas fa-star gray"></i>
+									</div>
+									<div class="price">
+										<p class="currency">Q.</p>
+										<p class="units">2,345.</p>
+										<p class="decimals">98</p>
+									</div>
+									<div class="legend">
+										<p>Precio de contado</p>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
+			</div>
+		</div>
+		<div class="banner boxed-width">
+			<div class="ban">
+				<img src="<?php echo SERVERURL; ?>vistas/assets/img/banner.jpg" alt="Banner">
 			</div>
 		</div>
     </main>
