46183489e83b70ecab13003d4c3c71f8f6ad457d
JoseAplitic <jose@aplitic.com.gt>
2019-04-09 22:52:49 -0600

    PureChat

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -2,5 +2,6 @@
         <script type="text/javascript" src="<?php echo SERVERURL; ?>vistas/js/slide.min.js"></script>
         <script type="text/javascript" src="<?php echo SERVERURL; ?>vistas/js/slick.min.js"></script>
         <script type="text/javascript" src="<?php echo SERVERURL; ?>vistas/js/scripts.js"></script>
+        <script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '4905b48e-061b-4a5a-ba0e-585680a59b1b', f: true }); done = true; } }; })();</script>
     </body>
 </html>
